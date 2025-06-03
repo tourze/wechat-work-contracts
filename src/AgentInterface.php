19d7e09b4fbb64e141873f889b2760cb7df0951a
tourze <tourze@users.noreply.github.com>
2025-06-03 21:07:36 +0800

    feat(contracts): 增加用户创建方法及Agent接口

---
+++ src/AgentInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tourze\WechatWorkContracts;
+
+interface AgentInterface
+{
+    public function getAgentId(): ?string;
+}
--- src/UserLoaderInterface.php
+++ src/UserLoaderInterface.php
@@ -8,4 +8,9 @@ interface UserLoaderInterface
      * 根据UserId和企业信息读取用户
      */
     public function loadUserByUserIdAndCorp(string $userId, CorpInterface $corp): ?UserInterface;
+
+    /**
+     * 创建用户
+     */
+    public function createUser(CorpInterface $corp, AgentInterface $agent, string $userId, string $name): UserInterface;
 }
